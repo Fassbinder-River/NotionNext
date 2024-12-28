bf577907c6df4b2b055e7b6fef9e9a83a4d40cd0
Fassbinder-River <101242254+Fassbinder-River@users.noreply.github.com>
2024-12-28 14:39:37 +0900

    Update JumpToTopButton.js

--- themes/movie/components/JumpToTopButton.js
+++ themes/movie/components/JumpToTopButton.js
@@ -1,18 +1,12 @@
 import { useGlobal } from '@/lib/global'
 
 /**
- * 跳转到网页顶部
- * 当屏幕下滑500像素后会出现该控件
- * @param targetRef 关联高度的目标html标签
- * @param showPercent 是否显示百分比
- * @returns {JSX.Element}
+ * 跳转到网页顶部按钮（不显示）
+ * @returns {JSX.Element|null}
  * @constructor
  */
 const JumpToTopButton = () => {
-  const { locale } = useGlobal()
-  return <div title={locale.POST.TOP} className='cursor-pointer p-2 text-center' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-    ><i className='fas fa-angle-up text-2xl' />
-    </div>
+  return null // 不渲染任何内容
 }
 
 export default JumpToTopButton
